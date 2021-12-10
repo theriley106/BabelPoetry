1a4b59e226e9da63bbd4497cb773eb5f5618a490
theriley106 <christopherlambert106@gmail.com>
2021-12-09 21:09:39 -0500

    Updated app

--- app.py
+++ app.py
@@ -1,12 +1,116 @@
 from flask import Flask, render_template, jsonify, redirect
 import json
 import random
+import parsegenerated
+import random
+import os
 
 app = Flask(__name__, static_url_path='/static/')
+
+
+poems = set()
+for val in json.load(open("poems.json"))['poems']:
+    poems.add(val['text'])
+
+poems = list(poems)
+GOOD_POEMS = open("good.txt").read().split("\n")
+BAD_POEMS = open("bad.txt").read().split("\n")
+
+DATASET = json.load(open("dataset.json"))
+to_rate = []
+
+for poem in DATASET['generated']:
+    if poem['id'] not in (GOOD_POEMS + BAD_POEMS):
+        to_rate.append(poem)
+
+def get_poem(generated=False, maxLength=500):
+    x = None
+    while x == None or (len(x) > maxLength or len(x) < 10):
+        if generated == True:
+            x = parsegenerated.generate()
+        else:
+            x = random.choice(poems)
+    return x
+
+@app.route('/api', methods=['GET'])
+def api():
+
+    x = [DATASET['by_id'].get(x, 'None') for x in open("good.txt").read().split("\n")]
+    return render_template("safe.html", values=x)
 
 def color():
     r = lambda: random.randint(0,255)
     return '#%02X%02X%02X' % (r(),r(),r())
+
+def mark_as_good(idVal):
+    os.system("echo '{}' >> good.txt".format(idVal))
+
+def mark_as_bad(idVal):
+    os.system("echo '{}' >> bad.txt".format(idVal))
+
+@app.route('/isGood/<idVal>', methods=['GET'])
+def is_good(idVal):
+    if 'good' in idVal:
+        idVal = idVal.replace("good", "")
+        mark_as_good(idVal)
+        print("MARKED AS GOOD")
+    else:
+        idVal = idVal.replace("bad", "")
+        mark_as_bad(idVal)
+        print("MARKED AS BAD")
+    
+    return "Okay"
+
+def poem_to_api_response(poem):
+    if poem['generated']:
+        about = """
+            This is an auto-generated poem by GPT-2
+        """
+        classVal = "generated"
+    else:
+        about = """
+            This is a poem called <b>Testin Poem</b>
+            written by <b>Testing Again</b>
+        """
+        classVal = "human"
+    
+    return {
+        "generated": poem['generated'],
+        "about": about,
+        "text": poem['text'],
+        "title": poem['title'],
+        "author": poem['author'],
+        "color": color(),
+        "class": classVal,
+        "id": poem['id'],
+
+    }
+
+def get_good_poem():
+    a = open("good.txt").read().split("\n")
+    b = "FAKE_KEY"
+    while b not in DATASET['by_id']:
+        b = random.choice(a)
+    return DATASET['by_id'][b]
+
+
+@app.route('/rate', methods=['GET'])
+def rate():
+    values = []
+    for i in range(10):
+        values.append(poem_to_api_response(to_rate.pop(0)))
+
+    return render_template("rate.html", values=values)
+
+@app.route('/onlyGood', methods=['GET'])
+def onlyGood():
+    values = []
+    for i in range(10):
+        x = poem_to_api_response(get_good_poem())
+        x['id_val'] = "about-{}".format(x['id'])
+        values.append(x)
+
+    return render_template("swipe.html", values=values)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,18 +122,37 @@ def index():
         This is a poem called <b>Testin Poem</b>
         written by <b>Testing Again</b>
     """
-    return render_template("swipe.html", values=[{
-        "generated": False,
-        "about": about,
-        "text": "Testing",
-        "title": "",
-        "author": "",
-        "color": color(),
-        "class": "generated",
-        "id_val": "about-{}".format(i),
-        "id": 24,
+    values = []
+    for i in range(10):
+        if random.randint(1,3) == 1:
+            values.append({
+                "generated": True,
+                "about": about,
+                "text": "generated" + get_poem(True),
+                "title": "",
+                "author": "",
+                "color": color(),
+                "class": "generated",
+                "id_val": "about-{}".format(i),
+                "id": 24,
 
-    } for i in range(10)])
+            })
+            
+        else:
+            values.append({
+                "generated": False,
+                "about": about,
+                "text": get_poem(False),
+                "title": "",
+                "author": "",
+                "color": color(),
+                "class": "",
+                "id_val": "about-{}".format(i),
+                "id": 24,
+
+            })
+
+    return render_template("swipe.html", values=values)
 
 @app.route('/new', methods=['GET'])
 def newQuestion():
